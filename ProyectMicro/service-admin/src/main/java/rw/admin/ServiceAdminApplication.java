333033e3d4e4c155cc5815073ca13ee9b41ac159
pedrogg <pedro.galan.guzman@gmail.com>
2023-04-10 14:46:03 +0200

    admin setup

---
+++ ProyectMicro/service-admin/src/main/java/rw/admin/ServiceAdminApplication.java
@@ -0,0 +1,13 @@
+package rw.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableA
+@SpringBootApplication
+public class ServiceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAdminApplication.class, args);
+	}
+
+}
---
+++ ProyectMicro/service-admin/src/test/java/rw/admin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
