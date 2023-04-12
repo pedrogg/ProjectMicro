c4475b618c7b0a96aa07e70e3d723a0957a18c61
pedrogg <pedro.galan.guzman@gmail.com>
2023-04-12 02:43:13 +0200

    admin fix

--- ProyectMicro/service-admin/src/main/java/rw/admin/ServiceAdminApplication.java
+++ ProyectMicro/service-admin/src/main/java/rw/admin/ServiceAdminApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @EnableAdminServer
 @SpringBootApplication
-public class ServiceAdminApplication {
+public class ServiceAdminApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAdminApplication.class, args);
