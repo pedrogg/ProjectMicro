2f6abe22f7220d3523035e7965e05899a7332867
pedrogg <pedro.galan.guzman@gmail.com>
2023-04-12 03:57:47 +0200

    Admin enables

--- ProyectMicro/admin/src/main/java/rw/admin/AdminApplication.java
+++ ProyectMicro/admin/src/main/java/rw/admin/AdminApplication.java
@@ -2,15 +2,18 @@ package rw.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
+@EnableDiscoveryClient
+@EnableScheduling
 @EnableAdminServer
 @SpringBootApplication
-public class ServiceAdminApplication  {
+public class AdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceAdminApplication.class, args);
+		SpringApplication.run(AdminApplication.class, args);
 	}
 
 }
